db815fa78306f08bbfaeac53495753e95a0bbd59
DuoRouLongShu <2957661828@qq.com>
2020-04-16 02:58:34 +0800

    第一天，就学了模块是啥，idea快捷键、idea结合GitHub

---
+++ day1/src/com/personal/Explaindemo1.java
@@ -0,0 +1,12 @@
+package com.personal;
+
+/**
+ * @description:
+ * @author:
+ * @time: 2020/4/16 1:50
+ */
+public class Explaindemo1 {
+    public static void main(String[] args) {
+        //ctrl + alt + l 格式化代码
+    }
+}
